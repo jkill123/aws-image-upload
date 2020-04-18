8251015145c53065edf266fa3ba005d90afbd84a
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-04-18 09:03:56 +0300

    added amazon s3 config

---
+++ src/main/java/com/pinchuk/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,30 @@
+package com.pinchuk.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Pinchuk Yevhen
+ * @created 17/04/2020
+ */
+@Configuration
+public class AmazonConfig {
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                "AKIAJGLB6ZAFHSUTFSRQ",
+                "45v4CHmFQSD9wSF6mBX2arJT/+s49vBRL6WoxbNj"
+        );
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
